565570612d8a94929f05cc12e303b66f236cd39f
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-24 14:07:59 -0600

    complete iteration 5

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,5 +1,22 @@
 require_relative "./node.rb"
 require_relative "./linked_list.rb"
 
-class JungleBeatList
-end 
+class JungleBeat
+
+  attr_reader :list
+
+  def initialize
+    @list = LinkedList.new
+  end
+
+  def append(beats)
+    beats = beats.split
+    beats.each do |beat|
+      list.append(beat)
+    end
+  end
+
+  def count
+    list.count 
+  end
+end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -5,19 +5,27 @@ require "./lib/jungle_beat.rb"
 
 class JungleBeatTest < Minitest::Test
   def test_it_exists
-    jb = JugleBeat.new
+    jb = JungleBeat.new
     assert_instance_of JungleBeat, jb
   end
 
   def test_it_has_attributes
-    jb = JugleBeat.new
+    jb = JungleBeat.new
     assert_equal LinkedList, jb.list.class
     assert_nil jb.list.head
   end
 
   def test_append
+    jb = JungleBeat.new
+    jb.append("deep doo ditt")
+    assert_equal "deep", jb.list.head.data
+    assert_equal "doo", jb.list.head.next.data
   end
 
   def test_count
-  end 
+    jb = JungleBeat.new
+    jb.append("deep doo ditt")
+    jb.append("woo hoo shu")
+    assert_equal 6, jb.count
+  end
 end
