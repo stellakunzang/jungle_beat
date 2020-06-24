b4a21ebba119a6a42e54c79212a157b8fcad6ab9
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-24 17:22:38 -0600

    add extension features to jungle beat class

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -4,19 +4,59 @@ require_relative "./linked_list.rb"
 class JungleBeat
 
   attr_reader :list
+  attr_accessor :voice, :rate
 
-  def initialize
+  def initialize(beat = nil)
     @list = LinkedList.new
+    @valid_beats = "tee dee deep bob boop la na ditt woo hoo shu doo bang dang clang hey ho hoo moo boo"
+    if beat != nil
+      @list.append(beat)
+    end
+    @voice = "Fiona"
+    @rate = 500
   end
 
   def append(beats)
     beats = beats.split
     beats.each do |beat|
-      list.append(beat)
+      if valid?(beat)
+        list.append(beat)
+      end
+    end
+  end
+
+  def prepend(beats)
+    beats = beats.split
+    beats.each do |beat|
+      if valid?(beat)
+        list.prepend(beat)
+      end
     end
   end
 
   def count
-    list.count 
+    list.count
+  end
+
+  def valid?(beat)
+    @valid_beats.include?(beat)
+  end
+
+  def play
+    beats = list.to_string
+    "say -r #{@rate.to_s} -v #{@voice} #{beats}"
+  end
+
+  def reset_rate
+    @rate = 500
+  end
+
+  def reset_voice
+    @voice = "Fiona"
+  end
+
+  def all
+    return 0 if list.head.nil?
+    list.to_string
   end
 end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -28,4 +28,51 @@ class JungleBeatTest < Minitest::Test
     jb.append("woo hoo shu")
     assert_equal 6, jb.count
   end
+
+  def test_play
+    jb = JungleBeat.new
+    jb.append("deep doo ditt woo hoo shu")
+    assert_equal 6, jb.count
+    assert_equal 6, jb.list.count
+    jb.play
+  end
+
+  def test_initialize_with_syllable
+    jb = JungleBeat.new("deep")
+    assert_equal 1, jb.count
+  end
+
+  def test_all
+    jb = JungleBeat.new
+    jb.append("deep doo ditt woo hoo shu")
+    assert_equal "deep doo ditt woo hoo shu", jb.all
+  end
+
+  def test_prepend
+    jb = JungleBeat.new("deep")
+    jb.prepend("tee tee tee Mississippi")
+    assert_equal "tee tee tee deep", jb.all
+  end
+
+  def test_validation
+    jb = JungleBeat.new
+    jb.append("Mississippi")
+    assert_equal 0, jb.all
+  end
+
+  def test_reset_voice
+    jb = JungleBeat.new
+    jb.voice = "Alice"
+    assert_equal "Alice", jb.voice
+    jb.reset_voice
+    assert_equal "Fiona", jb.voice
+  end
+
+  def test_reset_rate
+    jb = JungleBeat.new
+    jb.rate = 100
+    assert_equal 100, jb.rate
+    jb.reset_rate
+    assert_equal 500, jb.rate
+  end
 end
